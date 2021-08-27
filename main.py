849a7aeec10e20f51db55645f4471fdb7c5b357d
Nassos Kallipolitis <nasskall@unipi.gr>
2021-08-27 23:33:05 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # This is a sample Python script.
 import cv2
-from skimage import img_as_float
 from tensorflow.keras import backend as K
 import os
 import random
--- utils/SessionState.py
+++ utils/SessionState.py
@@ -1,27 +1,7 @@
-"""Hack to add per-session state to Streamlit.
-Usage
------
->>> import SessionState
->>>
->>> session_state = SessionState.get(user_name='', favorite_color='black')
->>> session_state.user_name
-''
->>> session_state.user_name = 'Mary'
->>> session_state.favorite_color
-'black'
-Since you set user_name above, next time your script runs this will be the
-result:
->>> session_state = get(user_name='', favorite_color='black')
->>> session_state.user_name
-'Mary'
-"""
-try:
-    import streamlit.ReportThread as ReportThread
-    from streamlit.server.Server import Server
-except Exception:
-    # Streamlit >= 0.65.0
-    import streamlit.report_thread as ReportThread
-    from streamlit.server.server import Server
+
+# Streamlit >= 0.65.0
+import streamlit.report_thread as ReportThread
+from streamlit.server.server import Server
 
 
 class SessionState(object):
